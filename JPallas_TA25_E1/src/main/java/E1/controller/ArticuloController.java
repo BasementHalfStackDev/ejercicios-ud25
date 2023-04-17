de401f0c9aafafc2ebc84f2aa6dd6ec5590f3074
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-04-17 15:10:46 +0200

    Fabricante controller done

---
+++ JPallas_TA25_E1/src/main/java/E1/controller/ArticuloController.java
@@ -0,0 +1,8 @@
+/**
+ * @author BasementHalfStackDev/Josep Maria Pallas Batalla
+ */
+package E1.controller;
+
+public class ArticuloController {
+
+}
---
+++ JPallas_TA25_E1/src/main/java/E1/controller/FabricanteController.java
@@ -0,0 +1,62 @@
+/**
+ * @author BasementHalfStackDev/Josep Maria Pallas Batalla
+ */
+package E1.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import E1.service.FabricanteService;
+import E1.dto.Fabricante;
+
+@RestController // Rest controller
+@RequestMapping("/api")
+public class FabricanteController {
+
+	// Implement service
+	@Autowired
+	FabricanteService fabricanteService;
+
+	@GetMapping("/fabricantes")
+	public List<Fabricante> listFabricantes() {
+		return fabricanteService.listFabricantes();
+	}
+
+	@PostMapping("/fabricantes")
+	public Fabricante saveFabricante(@RequestBody Fabricante fabricante) {
+		return fabricanteService.saveFabricante(fabricante);
+	}
+
+	@GetMapping("/fabricantes/{id}")
+	public Fabricante fabricanteById(@PathVariable(name = "id") Long id) {
+		Fabricante fabricantexID = new Fabricante();
+
+		fabricantexID = fabricanteService.FabricanteById(id);
+
+		return fabricantexID;
+	}
+
+	@PutMapping("/fabricantes/{id}")
+	public Fabricante updateFabricante(@PathVariable(name = "id") Long id, @RequestBody Fabricante fabricante) {
+		Fabricante selectedFabricante = new Fabricante(id, fabricante.getNombre());
+		Fabricante updatedFabricante = new Fabricante();
+
+		updatedFabricante = fabricanteService.updateFabricante(selectedFabricante);
+		return updatedFabricante;
+	}
+	
+	@DeleteMapping("/fabricanted/{id}")
+	public void deleteFabricante(@PathVariable(name = "id") Long id) {
+		fabricanteService.deleteFabricante(id);
+	}
+
+}
